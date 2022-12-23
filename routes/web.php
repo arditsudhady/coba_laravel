a12cab44578365d8bd18e2ba92de9f464f3b685b
arditsudhady <tsudhadyardi@gmail.com>
2022-12-23 20:29:06 +0700

    menambahkan dashboard dan autentikasi auth login

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,7 @@ Route::get('/categories', function() {
 //     ]);
 // });
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
 
 
