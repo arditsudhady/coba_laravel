b34f29b89ece005f6ffc0228d941360891dcee85
arditsudhady <tsudhadyardi@gmail.com>
2022-12-29 15:40:29 +0700

    menambahkan dashboard di bagian back end

---
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -0,0 +1,42 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="position-sticky pt-3 sidebar-sticky">
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/dashboard">
+            <span data-feather="home" class="align-text-bottom"></span>
+            Dashboard
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/dashboard/posts">
+            <span data-feather="file-text" class="align-text-bottom"></span>
+            My Posts
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <span data-feather="shopping-cart" class="align-text-bottom"></span>
+            Products
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <span data-feather="users" class="align-text-bottom"></span>
+            Customers
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <span data-feather="bar-chart-2" class="align-text-bottom"></span>
+            Reports
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <span data-feather="layers" class="align-text-bottom"></span>
+            Integrations
+          </a>
+        </li>
+      </ul>       
+    </div>
+  </nav>
--- routes/web.php
+++ routes/web.php
@@ -76,4 +76,6 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function() {
+    return view('dashboard.index');
+})->middleware('auth');
