063df79addb7e37d6c7193db156326ea43247768
arditsudhady <tsudhadyardi@gmail.com>
2023-01-02 15:37:35 +0700

    memodifikasii beberapaa dan menambahan sebuaah trix editor Laravel

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -16,11 +16,21 @@
                 <label for="slug" class="form-label">Slug</label>
                 <input type="text" class="form-control" id="slug" name="slug" disabled readonly>
             </div>
-            {{-- <div class="mb-3">
-                <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" id="title" name="title">
-            </div> --}}
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <div class="mb-3">
+                <label for="category" class="form-label">Category</label>
+                <select class="form-select" name="category">
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="body" class="form-label">Body</label>
+                <input id="body" type="hidden" name="body">
+                <trix-editor input="body"></trix-editor>
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Create Post</button>
         </form>
     </div>
 
@@ -34,5 +44,9 @@
                 .then(response => response.json())
                 .then(data => slug.value = data.slug)
         });
+
+        document.addEventListener('trix-file-accept'), function(e) {
+            e.preventDefault();
+        }
     </script>
 @endsection
