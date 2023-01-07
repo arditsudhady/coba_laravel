851c30e7b96cdb5b6601bc10046fdab7a0bd841a
arditsudhady <tsudhadyardi@gmail.com>
2023-01-07 19:50:29 +0700

    menambahkaan bagian lain dan memperbaiki validasi input data dalam post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -15,9 +15,6 @@
             <article class="my-3 fs-5">
                 {!! $post->body !!}
             </article>
-            
-
-            <a href="/blog" class="d-block mt-3">Back to Posts</a>
         </div>
     </div>
 </div>
