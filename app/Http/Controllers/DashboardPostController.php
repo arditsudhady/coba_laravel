93ad2050e5d1751cad84e3b86434c9eccce018aa
arditsudhady <tsudhadyardi@gmail.com>
2023-01-12 11:07:57 +0700

    menambahkan konfigurasi image

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -49,6 +49,7 @@ class DashboardPostController extends Controller
             'title'       => 'required|max:255',
             'slug'        => 'required|unique:posts',
             'category_id' => 'required',
+            'image'       => 'image|file|max:1024',
             'body'        => 'required'
         ]);
 
