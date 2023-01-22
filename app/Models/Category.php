f8dff5ddf4ad2e31e4b9aa1019cb4eb77a8b34e4
arditsudhady <tsudhadyardi@gmail.com>
2023-01-22 09:12:43 +0700

    menambahkan controller admin category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -15,4 +15,5 @@ class Category extends Model
     {
         return $this->hasMany(Post::class);
     }
+    
 }
--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -14,9 +14,9 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">
+          <a class="nav-link {{ Request::is('dashboard/categories*') ? 'active' : '' }}" href="/dashboard/categories">
             <span data-feather="shopping-cart" class="align-text-bottom"></span>
-            Products
+            Category
           </a>
         </li>
         <li class="nav-item">
